fbd9057869995603c64cc1c2c5c6d78b66c26cd7
Rayyan Alam <rayyanalam650@gmail.com>
2023-12-20 04:18:46 +0530

    feat(store): Implement session storage for  userId data persistence

--- providers/store.ts
+++ providers/store.ts
@@ -1,12 +1,22 @@
-import { Id } from "@/convex/_generated/dataModel";
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+
+import { Id } from "@/convex/_generated/dataModel";
 
 interface UserIdStoreState {
   userId: Id<"users"> | null;
   setUserId: (userId: Id<"users"> | null) => void;
 }
 
-export const useUserIdStore = create<UserIdStoreState>((set) => ({
-  userId: null,
-  setUserId: (userId) => set((state) => ({ userId })),
-}));
+export const useUserIdStore = create(
+  persist<UserIdStoreState>(
+    (set) => ({
+      userId: null,
+      setUserId: (userId) => set((state) => ({ userId })),
+    }),
+    {
+      name: "userId",
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
