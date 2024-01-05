8349f34ddf990e9a4d71f3d828f8123684d7c240
Rayyan Alam <rayyanalam650@gmail.com>
2024-01-06 02:27:52 +0530

    feat(types): Add 'document' types

--- types.d.ts
+++ types.d.ts
@@ -1,15 +1,22 @@
 export interface TopicTypes {
   topic: string;
-  by: "topic" | "paragraph";
+  by: "topic" | "paragraph" | "document";
   questions: number;
   format: "mcq" | "name" | "true_false";
 }
 
 export interface ParagraphTypes {
   paragraph: string;
-  by: "topic" | "paragraph";
+  by: "topic" | "paragraph" | "document";
   questions: number;
   format: "mcq" | "name" | "true_false";
 }
 
-export type QuestionType = TopicTypes | ParagraphTypes;
+export interface DocumentTypes {
+  document: FileList;
+  by: "topic" | "paragraph" | "document";
+  questions: number;
+  format: "mcq" | "name" | "true_false";
+}
+
+export type QuestionType = TopicTypes | ParagraphTypes | DocumentTypes;
