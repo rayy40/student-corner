969686d3bb139b46c5db548a8145d791c0f93860
Rayyan Alam <rayyanalam650@gmail.com>
2024-01-08 00:50:05 +0530

    feat(schema): Add initial quiz schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,10 +1,28 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
+const QuizFormat = v.union(
+  v.literal("mcq"),
+  v.literal("name"),
+  v.literal("true_false")
+);
+const QuizKind = v.union(
+  v.literal("topic"),
+  v.literal("paragraph"),
+  v.literal("document")
+);
+
 export default defineSchema({
   users: defineTable({
     userId: v.string(),
     email: v.string(),
     tokenIdentifier: v.string(),
   }).index("by_token", ["tokenIdentifier"]),
+  quiz: defineTable({
+    userId: v.id("users"),
+    questionNumber: v.number(),
+    content: v.string(),
+    format: QuizFormat,
+    kind: QuizKind,
+  }),
 });
