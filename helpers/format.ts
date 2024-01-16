bca6af60341e812919638a70b253da1ac2e2909b
Rayyan Alam <rayyanalam650@gmail.com>
2024-01-16 05:31:13 +0530

    feat(formats): Add OpenAI response formats

---
+++ helpers/format.ts
@@ -0,0 +1,23 @@
+export const MCQformat = [
+  {
+    question: "Who is Luke Skywalker's father?",
+    options: ["Obi-Wan Kenobi", "Emperor Palpatine", "Darth Vadar", "Yoda"],
+    answer: "Darth Vadar",
+  },
+];
+
+export const NameTheFollowingformat = [
+  {
+    question: "Name the device that is used to find directions",
+    answer: "Compass",
+  },
+];
+
+export const TrueFalseformat = [
+  {
+    question:
+      "Amphibians are cold-blooded animals, meaning their body temperature varies with their environment.",
+    options: ["True", "False"],
+    answer: "True",
+  },
+];
