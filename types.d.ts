45278278a3931ff93ef9f64cc15d97962ffe7204
Rayyan Alam <rayyanalam650@gmail.com>
2024-01-20 04:56:44 +0530

    refactor(types): Refactored types for better readability

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,6 @@
+import { Infer } from "convex/values";
+import { Response } from "./convex/schema";
+
 export interface TopicTypes {
   topic: string;
   by: "topic" | "paragraph" | "document";
@@ -29,3 +32,36 @@ export type SelectedOptions = {
   key: number;
   value: string;
 }[];
+
+export type ResponseData = Infer<typeof Response>;
+
+export type GameData = {
+  _id: Id<"quiz">;
+  _creationTime: number;
+  content: string;
+  format: "mcq" | "name" | "true_false";
+  kind: "topic" | "paragraph" | "document";
+  questionNumber: number;
+  response?: ResponseData | string;
+  result?: {
+    correctAnswer: number;
+    score: number;
+  };
+  title?: string;
+  userId: Id<"users">;
+};
+
+interface ResponseType {
+  fallbackData: GameData | undefined;
+  quizData: GameData | undefined | null;
+  isGeneratingQuiz: boolean;
+  invalidQuizId: boolean;
+  idNotFound: boolean;
+}
+
+interface QuizData {
+  yourAnswer?: string | undefined;
+  options?: string[] | undefined;
+  question: string;
+  answer: string;
+}
