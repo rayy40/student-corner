bed1690288b5beb7652a1604093b62f67dfd0094
Rayyan Alam <rayyanalam650@gmail.com>
2024-01-23 03:14:40 +0530

    style(dashboard/card): Add whitespace to button element

--- components/DashboardCard/DashboardCard.tsx
+++ components/DashboardCard/DashboardCard.tsx
@@ -11,26 +11,26 @@ interface Props {
 
 const DashboardCard = ({ topic, questions, quizId, score }: Props) => {
   return (
-    <div className="flex flex-col items-start border rounded-lg border-border shadow-medium">
-      <div className="max-w-[250px] font-dmSans rounded-t-md p-5 text-2xl">
+    <div className="flex flex-col items-start justify-between border rounded-lg border-border shadow-medium">
+      <div className="max-w-[280px] font-dmSans rounded-t-md px-4 py-5 text-2xl">
         {topic}
       </div>
-      <div className="flex items-center gap-2 px-5 pb-5 text-lg divide-x text-muted-foreground">
+      <div className="flex items-center gap-2 px-4 pb-5 text-lg divide-x text-muted-foreground">
         <p> {questions} Questions </p>
         <p className="pl-2"> Score - {score} </p>
       </div>
-      <div className="flex items-center w-full gap-4 p-5 bg-border text-secondary-foreground">
+      <div className="flex items-center w-full gap-4 p-4 py-5 bg-border text-secondary-foreground">
         <Link
           className="w-1/2 p-4 px-2 text-center transition-colors border rounded-lg border-muted hover:bg-secondary-hover"
           href={`/quiz/${quizId}`}
         >
-          <button>Take Quiz Again</button>
+          <button className="whitespace-nowrap">Take Quiz Again</button>
         </Link>
         <Link
           className="w-1/2 p-4 px-2 text-center transition-colors border rounded-lg border-muted hover:bg-secondary-hover"
           href={`/quiz/${quizId}/result`}
         >
-          <button>Show Statistics</button>
+          <button className="whitespace-nowrap">Show Statistics</button>
         </Link>
       </div>
     </div>
