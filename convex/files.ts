2124e3810485749459defcc125c80cd9aba9a996
Rayyan Alam <rayyanalam650@gmail.com>
2024-01-23 03:15:28 +0530

    feat(files): Add a function for uploading of files

---
+++ convex/files.ts
@@ -0,0 +1,16 @@
+import { v } from "convex/values";
+import { internalQuery, mutation } from "./_generated/server";
+
+export const generateUploadUrl = mutation({
+  handler: async (ctx) => {
+    return await ctx.storage.generateUploadUrl();
+  },
+});
+
+export const getFileUrl = internalQuery({
+  args: { storageId: v.id("_storage") },
+  handler: async (ctx, args) => {
+    const url = await ctx.storage.getUrl(args.storageId);
+    return url ?? "No Url Found.";
+  },
+});
