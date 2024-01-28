ea907dbb1705e34ef04080875c9446205f5b2374
Rayyan Alam <rayyanalam650@gmail.com>
2024-01-28 20:49:55 +0530

    style(color): Update text color

--- components/Upload/Documents/Documents.tsx
+++ components/Upload/Documents/Documents.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { LuFileUp } from "react-icons/lu";
 import { FieldError } from "react-hook-form";
+import { LuFileUp } from "react-icons/lu";
+
 import { DocumentType } from "@/types";
 
 const Document = <K extends string>({
@@ -11,7 +12,7 @@ const Document = <K extends string>({
 }: DocumentType<K>) => {
   return (
     <div className="flex flex-col gap-1">
-      <div className="font-semibold text-[hsl(0_0%_50%)]">Document</div>
+      <div className="font-semibold text-label">Document</div>
       <div className="p-2 w-full h-[150px] bg-input-background rounded-md shadow-[inset_0_0_6px_-2px_rgba(15,15,15,0.25)]">
         <div className="cursor-pointer relative bg-input rounded-md border-2 bg-light-gray border-dotted border-dark-gray w-full h-full flex items-center justify-center shadow-[inset_0_0_6px_-2px_rgba(15,15,15,0.25)]">
           <label
