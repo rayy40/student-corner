3c75ef839bc1738d8b7fefd6a032d8573870a064
Rayyan Alam <rayyanalam650@gmail.com>
2024-01-28 20:47:19 +0530

    feat(stream): Use langchain with Vercel AI.

--- app/api/chat/route.tsx
+++ app/api/chat/route.tsx
@@ -1,22 +1,44 @@
-import { Configuration, OpenAIApi } from "openai-edge";
-import { OpenAIStream, StreamingTextResponse } from "ai";
+import { Message, StreamingTextResponse } from "ai";
+import { PromptTemplate } from "langchain/prompts";
+import { BytesOutputParser } from "langchain/schema/output_parser";
+import { ChatOpenAI } from "langchain/chat_models/openai";
 
 export const runtime = "edge";
 
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(config);
+const formatMessage = (message: Message) => {
+  return `${message.role}: ${message.content}`;
+};
+
+const TEMPLATE = `You are a helpful assistant. Your task is to answer questions related to the context provided to you. Please STRICTLY ADHERE to answering questions related to the content only and if a question is unrelated then refrain from giving an answer even if you know the answer. KEEP IN MIND.
+ 
+Current conversation:
+{chat_history}
+ 
+User: {input}
+AI:`;
 
 export async function POST(req: Request) {
   try {
     const { messages } = await req.json();
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      stream: true,
-      messages,
+    const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
+    const currentMessageContent = messages[messages.length - 1].content;
+
+    const prompt = PromptTemplate.fromTemplate(TEMPLATE);
+
+    const model = new ChatOpenAI({
+      temperature: 0.8,
+      modelName: "gpt-3.5-turbo",
+      openAIApiKey: process.env.OPENAI_API_KEY,
     });
-    const stream = OpenAIStream(response);
+
+    const outputParser = new BytesOutputParser();
+
+    const chain = prompt.pipe(model).pipe(outputParser);
+
+    const stream = await chain.stream({
+      chat_history: formattedPreviousMessages.join("\n"),
+      input: currentMessageContent,
+    });
 
     return new StreamingTextResponse(stream);
   } catch (error) {
