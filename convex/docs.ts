874e386432236087b4312ea5bc395379072f2123
Rayyan Alam <rayyanalam650@gmail.com>
2024-01-28 08:23:20 +0530

    feat(audio): Add logic to extract audio from url and create chunks

--- convex/docs.ts
+++ convex/docs.ts
@@ -1,14 +1,25 @@
 "use node";
 
 import { v } from "convex/values";
-import { internalAction } from "./_generated/server";
-import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { Document } from "langchain/document";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { internal } from "./_generated/api";
+import { OpenAI, toFile } from "openai";
+import { YoutubeTranscript } from "youtube-transcript";
 
-const createChunks = async (url: string) => {
+import { internal } from "./_generated/api";
+import { internalAction } from "./_generated/server";
+
+const ytdl = require("ytdl-core");
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const extractText = async (url: string) => {
   try {
+    let document: string[] = [];
+
     const response: Response = await fetch(url);
     const blob: Blob = await response.blob();
     const loader: PDFLoader = new PDFLoader(blob);
@@ -16,23 +27,85 @@ const createChunks = async (url: string) => {
 
     const metadata = docs[0].metadata;
 
+    docs.forEach((doc) => {
+      document.push(doc.pageContent);
+    });
+
+    return { document, metadata };
+  } catch (error) {
+    console.error("Error: ", error);
+    return error as string;
+  }
+};
+
+const extractAudio = async (url: string) => {
+  try {
+    const videoId = ytdl.getURLVideoID(url);
+
+    if (!ytdl.validateID(videoId)) {
+      throw new Error("Invalid video Id.");
+    }
+
+    const metadata = await ytdl.getBasicInfo(videoId);
+
+    let transcript: string = "";
+    try {
+      const response = await YoutubeTranscript.fetchTranscript(videoId);
+      transcript += response.map((text) => text.text);
+    } catch (error) {
+      console.warn(
+        "Transcript fetch failed, falling back to audio transcription"
+      );
+
+      const audioStream = await ytdl(url, { filter: "audioonly" });
+      if (!audioStream) {
+        throw new Error("Unable to extract audio stream from video.");
+      }
+
+      const response = await openai.audio.transcriptions.create({
+        file: await toFile(audioStream, "myfile.mp3"),
+        model: "whisper-1",
+      });
+      transcript = response.text;
+    }
+    return { transcript, metadata };
+  } catch (error) {
+    console.error("Error: ", error);
+    return error as string;
+  }
+};
+
+const createChunks = async (
+  url: string,
+  kind: "pdf" | "audio"
+): Promise<{ chunks: string[]; metadata: any } | string> => {
+  try {
+    const extractTextFn = kind === "pdf" ? extractText : extractAudio;
+    const response = await extractTextFn(url);
+
+    if (typeof response === "string") {
+      throw new Error(response);
+    }
+
+    let docs: string[];
+
+    if ("document" in response) {
+      docs = response.document;
+    } else {
+      docs = [response.transcript];
+    }
+
     const splitter = new RecursiveCharacterTextSplitter({
       separators: ["\n"],
       chunkSize: 1000,
       chunkOverlap: 200,
     });
 
-    let document: string[] = [];
-
-    docs.forEach((doc) => {
-      document.push(doc.pageContent);
-    });
-
-    const chunks = await splitter.createDocuments(document);
+    const chunks = await splitter.createDocuments(docs);
 
     const chunkArr = chunks.map((chunk) => chunk.pageContent);
 
-    return { chunks: chunkArr, metadata: metadata };
+    return { chunks: chunkArr, metadata: response.metadata };
   } catch (error) {
     console.log(error);
     return "Unable to parse through file.";
@@ -40,10 +113,14 @@ const createChunks = async (url: string) => {
 };
 
 export const extractTextAndCreateChunks = internalAction({
-  args: { url: v.string(), chatId: v.id("chat") },
+  args: {
+    url: v.string(),
+    chatId: v.id("chat"),
+    kind: v.union(v.literal("pdf"), v.literal("audio")),
+  },
   handler: async (ctx, args) => {
     try {
-      const chunks = await createChunks(args.url);
+      const chunks = await createChunks(args.url, args.kind);
 
       if (typeof chunks == "string") {
         throw new Error(chunks);
