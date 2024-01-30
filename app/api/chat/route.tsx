8ddcdd436385f395286d9433796ba25d3af46267
Rayyan Alam <rayyanalam650@gmail.com>
2024-01-31 02:32:10 +0530

    fix(chat/route): Add messages to database and add prompt

--- app/api/chat/route.tsx
+++ app/api/chat/route.tsx
@@ -1,48 +1,78 @@
-import { Message, StreamingTextResponse } from "ai";
-import { PromptTemplate } from "langchain/prompts";
-import { BytesOutputParser } from "langchain/schema/output_parser";
-import { ChatOpenAI } from "langchain/chat_models/openai";
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse, Message } from "ai";
+import { fetchAction, fetchMutation } from "convex/nextjs";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { generateRandomString } from "@/helpers/utils";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 export const runtime = "edge";
 
-const formatMessage = (message: Message) => {
-  return `${message.role}: ${message.content}`;
-};
+interface PatchMessage {
+  chatId: Id<"chatbook">;
+  role: "user" | "assistant";
+  content: string;
+}
 
-const TEMPLATE = `You are a helpful assistant. Your task is to answer questions related to the context provided to you. Please STRICTLY ADHERE to answering questions related to the content only and if a question is unrelated then refrain from giving an answer even if you know the answer. KEEP IN MIND.
- 
-Current conversation:
-{chat_history}
- 
-User: {input}
-AI:`;
+export const patchMessages = async ({
+  chatId,
+  role,
+  content,
+}: PatchMessage) => {
+  await fetchMutation(api.chatbook.patchMessages, {
+    chatId,
+    message: {
+      id: generateRandomString(),
+      content,
+      role,
+    },
+  });
+};
 
 export async function POST(req: Request) {
   try {
-    const { messages } = await req.json();
-    const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
-    const currentMessageContent = messages[messages.length - 1].content;
+    const { messages, chatId } = await req.json();
+    const previousMessage: string = messages[messages.length - 1].content;
 
-    const prompt = PromptTemplate.fromTemplate(TEMPLATE);
-
-    const model = new ChatOpenAI({
-      temperature: 0.8,
-      modelName: "gpt-3.5-turbo",
-      openAIApiKey: process.env.OPENAI_API_KEY,
+    const content = await fetchAction(api.chatbook.similarContent, {
+      chatId,
+      query: previousMessage,
     });
 
-    const outputParser = new BytesOutputParser();
+    const prompt = {
+      role: "system",
+      content: `You are a helpful assistant. Your task is to answer questions related to the context provided to you. Please STRICTLY ADHERE to answering questions related to the content only and if a question is unrelated then refrain from giving an answer even if you know the answer and say 'I'm sorry but I do not know the answer to this query as it is unrelated to the content'. You will not apologize for previous responses. And process the response intelligently into markdown formatting for structure, without altering the contents.
+  
+      START CONTENT BLOCK
+      ${content}
+      END CONTENT BLOCK
+    `,
+    };
 
-    const chain = prompt.pipe(model).pipe(outputParser);
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      stream: true,
+      messages: [
+        prompt,
+        ...messages.filter((message: Message) => message.role === "user"),
+      ],
+    });
 
-    const stream = await chain.stream({
-      chat_history: formattedPreviousMessages.join("\n"),
-      input: currentMessageContent,
+    const stream = OpenAIStream(response, {
+      onStart: async () => {
+        await patchMessages({ chatId, role: "user", content: previousMessage });
+      },
+      onCompletion: async (completion) => {
+        await patchMessages({ chatId, role: "assistant", content: completion });
+      },
     });
 
     return new StreamingTextResponse(stream);
   } catch (error) {
     console.log(error);
-    return "Error while generating response from Open AI.";
+    return "Unable to get response.";
   }
 }
