8e38b4dcd92a48b7bfb88580235064d4803a7dab
Rayyan Alam <rayyanalam650@gmail.com>
2024-02-06 01:51:57 +0530

    feat(table): Add table style for markdown in chatdown

---
+++ components/Markdown/Table.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+
+interface TableProps {
+  children: JSX.Element | JSX.Element[];
+}
+
+const Table = ({ children }: TableProps) => {
+  const TableData = ({ child }: { child: JSX.Element }) => {
+    return <td className="flex-1 p-3">{child["props"]?.["children"]?.[0]}</td>;
+  };
+  const TableHead = ({ child }: { child: JSX.Element }) => {
+    return (
+      <th className="flex-1 font-medium p-3 capitalize ">
+        {child["props"]?.["children"]?.[0]}
+      </th>
+    );
+  };
+
+  const TableRow = ({
+    child,
+    type,
+  }: {
+    child: JSX.Element | JSX.Element[];
+    type: "head" | "body";
+  }) => {
+    return (
+      <tr className="divide-x divide-[hsl(0_0_84%)] flex justify-start items-start">
+        {type === "head" ? (
+          Array.isArray(child) ? (
+            child.map((item, index) => <TableHead key={index} child={item} />)
+          ) : (
+            <TableHead child={child} />
+          )
+        ) : Array.isArray(child) ? (
+          child.map((item, index) => <TableData key={index} child={item} />)
+        ) : (
+          <TableData child={child} />
+        )}
+      </tr>
+    );
+  };
+
+  const TableHeader = ({ child }: { child: JSX.Element | JSX.Element[] }) => {
+    return (
+      <thead className=" border-b border-b-[hsl(0_0_84%)] bg-[hsl(0_0_88%)] text-secondary-foreground">
+        {Array.isArray(child) ? (
+          child.map((item, index) => (
+            <TableRow
+              child={item["props"]?.["children"]}
+              type={"head"}
+              key={index}
+            />
+          ))
+        ) : (
+          <TableRow child={child["props"]?.["children"]} type={"head"} />
+        )}
+      </thead>
+    );
+  };
+
+  const TableBody = ({ child }: { child: JSX.Element | JSX.Element[] }) => {
+    return (
+      <tbody className="divide-y divide-[hsl(0_0_84%)]">
+        {Array.isArray(child) ? (
+          child.map((item, index) => (
+            <TableRow
+              child={item["props"]?.["children"]}
+              type={"body"}
+              key={index}
+            />
+          ))
+        ) : (
+          <TableRow child={child["props"]?.["children"]} type={"body"} />
+        )}
+      </tbody>
+    );
+  };
+
+  return (
+    <table className="border border-[hsl(0_0_84%)] rounded-lg">
+      {Array.isArray(children) &&
+        children.map((item, index) => {
+          if (item.type === "thead") {
+            return (
+              <TableHeader key={index} child={item["props"]?.["children"]} />
+            );
+          } else if (item.type === "tbody") {
+            return (
+              <TableBody key={index} child={item["props"]?.["children"]} />
+            );
+          }
+          return null;
+        })}
+    </table>
+  );
+};
+
+export default Table;
