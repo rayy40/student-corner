c31788c51da7fbf37500e3701c3d0a55c809c486
Rayyan Alam <rayyanalam650@gmail.com>
2024-02-15 00:14:01 +0530

    feat: Add logic to render sidebar depending on the rag model

--- app/chat/[chatId]/page.tsx
+++ app/chat/[chatId]/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 
 import React from "react";
-import Youtube from "react-youtube";
 
-import ChatBot from "@/components/ChatBot/ChatBot";
-import LoadingSpinner from "@/components/Loading/LoadingSpinner";
-import PDFViewer from "@/components/PDFViewer/PDFViewer";
+import ChatBot from "@/components/ChatBot";
+import LoadingSpinner from "@/components/LoadingSpinner";
+import PDFViewer from "@/components/PDFViewer";
+import UnAuthenticated from "@/components/UnAuthenticated";
+import VideoViewer from "@/components/VideoViewer";
 import { Id } from "@/convex/_generated/dataModel";
 import { useQueryChatProps } from "@/hooks/useQueryObject";
+import { useAuth } from "@clerk/clerk-react";
+import CodeViewer from "@/components/CodeViewer";
 
 const ChatId = ({ params }: { params: { chatId: string } }) => {
+  const { isLoaded, isSignedIn } = useAuth();
   const chat = useQueryChatProps({ chatId: params.chatId as Id<"chatbook"> });
+
+  if (isLoaded && !isSignedIn) {
+    return <UnAuthenticated />;
+  }
 
   if (chat.loading) {
     return (
@@ -22,12 +30,19 @@ const ChatId = ({ params }: { params: { chatId: string } }) => {
 
   return (
     <div className="h-[calc(100vh-56px)] mt-14 font-sans flex">
-      <div className="h-full flex-[6] border-r border-r-border">
-        {/* <Youtube videoId="SSjdRXwqg_U" /> */}
-        {/* <PDFViewer url={chat?.url} /> */}
+      <div className="h-full w-[60%] border-r border-r-border">
+        {chat?.data?.type === "code" && (
+          <CodeViewer url={chat?.data?.url} chatId={chat?.data?._id} />
+        )}
+        {chat?.data?.type === "video" && <VideoViewer url={chat?.data?.url} />}
+        {chat?.data?.type === "doc" && <PDFViewer url={chat?.data?.url!} />}
       </div>
-      <div className="flex justify-between flex-col h-full min-w-[450px] flex-[4]">
-        <ChatBot chatId={chat?.data?._id!} title={chat?.data?.title!} />
+      <div className="flex w-[40%] justify-between flex-col h-full min-w-[450px]">
+        <ChatBot
+          chatId={chat?.data?._id!}
+          title={chat?.data?.title!}
+          type={chat.data?.type}
+        />
       </div>
     </div>
   );
