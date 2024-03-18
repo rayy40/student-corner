0bfb54bef29e0a1007bd1af1205d7bac0975b648
Rayyan Alam <rayyanalam650@gmail.com>
2024-03-19 04:50:25 +0530

    fix(error): set to variable and then exported

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,4 +1,4 @@
-export default {
+const auth = {
   providers: [
     {
       domain: "https://caring-grizzly-77.clerk.accounts.dev",
@@ -6,3 +6,5 @@ export default {
     },
   ],
 };
+
+export default auth;
