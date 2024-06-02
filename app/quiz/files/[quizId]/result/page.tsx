9a880c099d99170a4ff5d6adf8d13bd1cfbbf61a
Rayyan Alam <rayyanalam650@gmail.com>
2024-06-02 22:57:08 +0530

    feat: add try again button in result page

--- app/quiz/files/[quizId]/result/page.tsx
+++ app/quiz/files/[quizId]/result/page.tsx
@@ -6,15 +6,14 @@ import { Id } from "@/convex/_generated/dataModel";
 import { getQuiz } from "@/db/quiz";
 
 const Page = async ({ params }: { params: { quizId: string } }) => {
+  const quizId = params.quizId as Id<"quiz">;
   const session = await auth();
 
   if (!session?.user?.id) {
     return <UnAuthenticated />;
   }
 
-  const { loading, success, error } = await getQuiz(
-    params.quizId as Id<"quiz">
-  );
+  const { loading, success, error } = await getQuiz(quizId);
 
   if (loading) {
     return <Loading />;
@@ -29,6 +28,7 @@ const Page = async ({ params }: { params: { quizId: string } }) => {
       score={success?.score}
       numberOfQuestions={success?.numberOfQuestions}
       questions={success?.response?.questions}
+      id={quizId}
     />
   );
 };
